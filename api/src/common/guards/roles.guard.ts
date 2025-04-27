6c023b3969064f95355e670480e6bdf22d5e7963
Juan Ignacio Colli <juanignaciocolli28@gmail.com>
2025-04-27 15:06:57 -0300

    Implement: Decorator roles to make some routes private for admins only

---
+++ api/src/common/guards/roles.guard.ts
@@ -0,0 +1,30 @@
+// src/common/guards/roles.guard.ts
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true; // Si no hay roles requeridos, permitir acceso
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    if (!user || !user.role) {
+      console.log(user);
+      
+      return false; // No hay usuario o no tiene rol
+    }
+
+    return requiredRoles.some((role) => user.role === role);
+  }
+}
