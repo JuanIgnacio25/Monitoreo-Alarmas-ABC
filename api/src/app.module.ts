fb6960e9545daa2dd7869dce28954d5b648c2573
Juan Ignacio Colli <juanignaciocolli28@gmail.com>
2025-04-30 11:16:22 -0300

    Config Module cookie-parser

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
