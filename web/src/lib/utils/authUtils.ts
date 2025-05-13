f5bb0117ebd223df67a2623d46f1479b610199a2
Juan Ignacio Colli <juanignaciocolli28@gmail.com>
2025-05-13 08:35:04 -0300

    Add: auth util

---
+++ web/src/lib/utils/authUtils.ts
@@ -0,0 +1,25 @@
+import { jwtDecode } from 'jwt-decode';
+
+interface DecodedToken {
+  userId: number,
+  email: string,
+  role: string,
+  exp: number;
+}
+
+export const isTokenExpiredOrAboutToExpire = (accessToken: string | null, expirationThresholdSeconds: number = 60): boolean => {
+  if (!accessToken) {
+    return true;
+  }
+  try {
+    const decodedToken = jwtDecode<DecodedToken>(accessToken);
+  
+    const currentTime = Math.floor(Date.now() / 1000);
+    
+    // Comprueba si el token ha expirado o expirará en los próximos 'expirationThresholdSeconds' segundos
+    return decodedToken.exp < currentTime + expirationThresholdSeconds;
+  } catch (error) {
+    // El token no es válido o no se puede decodificar
+    return true;
+  }
+};
