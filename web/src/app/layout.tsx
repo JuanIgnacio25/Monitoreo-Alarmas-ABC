de22114b42a9ae646d19332f4494b953b12b86e8
Juan Ignacio Colli <juanignaciocolli28@gmail.com>
2025-05-21 14:39:25 -0300

    Add: font montserrat

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,17 +1,15 @@
 import type { Metadata } from "next";
-/* import { Geist, Geist_Mono } from "next/font/google"; */
+import { Montserrat } from 'next/font/google'
+
 import "./globals.css";
 import Navbar from "@/components/navbar/NavBar";
 
-/* const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-}); */
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+  display: 'swap',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={montserrat.className}>
       <body
       >
         <Navbar/>
